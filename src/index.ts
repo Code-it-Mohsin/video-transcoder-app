ee2e9d4896455b92ecf381b5434c505f66ad26ea
Mrrobot-002 <mohsinshk002@gmail.com>
2026-09-12 20:49:36 +0530

    AWS Queues S3 Roles, etc created

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
 console.log('Config complete')
+
+import "dotenv/config"
+import { SQSClient } from "@aws-sdk/client-sqs"
+
+const sqsConfig = {
+  credentials:{
+    accessKeyId: process.env.AWS_ADMIN_ACCESS_KEY!,
+    secretAccessKey: process.env.AWS_ADMIN_SECRET_KEY!,
+  }
+}
+const sqsClient = new SQSClient(sqsConfig)
+
+async function init(){
+  console.log(`sqsClient is running`)
+}
+init()
